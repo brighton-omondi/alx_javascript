21ac0d4a17209809795d18392aa8ecae373a10aa
brighton-omondi <brightonomondi@yahoo.com>
2023-10-19 17:07:49 +0300

    README update

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,14 @@
+#!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-    
-        return {};
-      }
-  
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer
+      Object.create(null);
     }
   }
-  
+}
+
+module.exports = Rectangle;
